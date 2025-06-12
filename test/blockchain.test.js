4bd1fec0fa3188a6dc3856e3ff88e812eb5723aa
sara-poker <spoker1412@gmail.com>
2025-06-12 15:51:45 +0330

    write Test for blockchain

---
+++ test/blockchain.test.js
@@ -0,0 +1,20 @@
+const Block = require('../code/block')
+const Blockchain = require('../code/blockchain')
+
+describe('Blockchain', () => {
+    const blockchain = new Blockchain();
+
+    it('containes a `chain` Array instance', () => {
+        expect(blockchain.chain instanceof Array).toBe(true)
+    })
+
+    it('starts with the genesis block', () => {
+        expect(blockchain.chain[0]).toEqual(Block.genesis())
+    })
+
+    it('add new block to the chain', () => {
+        const newData = 'foo bar'
+        blockchain.addBlock({ data: newData });
+        expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData)
+    })
+})
