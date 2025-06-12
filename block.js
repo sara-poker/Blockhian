55d8aad639d9d9ed17ba4cdc47e0eef03eea2973
sara-poker <spoker1412@gmail.com>
2025-06-12 15:16:13 +0330

    add mineBlock method

--- block.js
+++ block.js
@@ -8,8 +8,16 @@ class Block {
         this.data = data
     }
 
-    static genesis(){
+    static genesis() {
         return new this(GENESIS_DATA)
+    }
+
+    static mineBlock({ lastBlock, data }) {
+        return new this({
+            timestamp: Date.now(),
+            lastHash: lastBlock.hash,
+            data: data
+        })
     }
 }
 
